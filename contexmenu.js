91bce1a388eb110fbf423cc9f54e68f62be8f313
Kevin M <crisstian2345@gmail.com>
2023-09-04 09:00:31 -0500

    agrega layout para editar un row

--- contexmenu.js
+++ contexmenu.js
@@ -1,18 +1,12 @@
-// td.forEach(e => {
-//     console.log(e);
-// })
-
-let posX;
-let posY;
+let posX, posY;
+let select_elmnt = null;
 
 tables.forEach(table => {
-    // console.log(table);
     table.addEventListener('contextmenu', function(e) {
         e.preventDefault();
         const target = e.target;
         posX = e.x;
         posY = e.y;
-        // console.log(e);
         if(target.tagName == "TH"){
             handle_th(target);
         }
@@ -25,19 +19,22 @@ tables.forEach(table => {
 
 
 function handle_th(elmnt) {
-    console.log(elmnt);
     cm_th.style.left = posX + "px";
     cm_th.style.top = posY + "px";
     cm_th.style.display = "block";
+    select_elmnt = elmnt;
 }
 
 function handle_td(elmnt) {
-    console.log(elmnt);
     cm_td.style.left = posX + "px";
     cm_td.style.top = posY + "px";
     cm_td.style.display = "grid";
+    select_elmnt = elmnt;
 }
 
+/**
+ * para que desaparezcan los div context menu
+ */
 
 cm_th.querySelector('div').addEventListener('mouseleave', function(e) {
     if(e.relatedTarget.tagName !== "BUTTON"){
@@ -49,4 +46,37 @@ cm_td.querySelector('div').addEventListener('mouseleave', function(e) {
     if(e.relatedTarget.tagName !== "BUTTON"){
         cm_td.style.display = "none";
     }
+});
+
+
+/**
+ * funcionalidad para el edit_row
+ */
+
+edit_row.addEventListener('click', function(e) {
+    cm_td.style.display = "none";
+    margin_edit_out.style.display = "block";
+    edit_layout.style.display = "grid";
+    document.body.style.overflow = "hidden";
+
+    if(select_elmnt != null){
+        const parent_th = select_elmnt.parentElement;
+        name_text.setAttribute("placeholder", parent_th.children[0].textContent);
+        link_text.setAttribute("placeholder", parent_th.children[1].textContent);
+
+        // note_text.setAttribute("placeholder", parent_th.children[].textContent);
+    }
+});
+
+cancel_edit_btn.addEventListener('click', () => {
+    margin_edit_out.style.display = "none";
+    edit_layout.style.display = "none";
+    document.body.style.overflow = "auto";
+});
+
+submit_edit_btn.addEventListener('click', () => {
+    margin_edit_out.style.display = "none";
+    edit_layout.style.display = "none";
+    document.body.style.overflow = "auto";
+
 });
--- references.js
+++ references.js
@@ -1,4 +1,19 @@
 const td = document.querySelectorAll('td');
 const tables = document.querySelectorAll('table');
+
 const cm_th = document.getElementById('context_menu_th');
 const cm_td = document.getElementById('context_menu_td');
+
+const check_status = document.getElementById('check_status');
+
+const edit_row = document.getElementById('edit_row');
+const delete_row = document.getElementById('delete_row');
+const get_info = document.getElementById('get_info');
+
+const margin_edit_out = document.getElementById('margin_edit_out');
+const edit_layout = document.getElementById('edit_layout');
+const name_text = document.getElementById('name_text');
+const link_text = document.getElementById('link_text');
+const note_text = document.getElementById('note_text');
+const cancel_edit_btn = document.getElementById('cancel_edit_btn');
+const submit_edit_btn = document.getElementById('submit_edit_btn');
