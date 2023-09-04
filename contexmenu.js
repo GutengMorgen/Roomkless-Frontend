56243fc81e1ffdebab0f579fde376979f29d6247
Kevin M <crisstian2345@gmail.com>
2023-09-04 07:14:47 -0500

    agrega contextmenu

--- contexmenu.js
+++ contexmenu.js
@@ -2,11 +2,16 @@
 //     console.log(e);
 // })
 
+let posX;
+let posY;
+
 tables.forEach(table => {
-    console.log(table);
+    // console.log(table);
     table.addEventListener('contextmenu', function(e) {
         e.preventDefault();
         const target = e.target;
+        posX = e.x;
+        posY = e.y;
         // console.log(e);
         if(target.tagName == "TH"){
             handle_th(target);
@@ -21,8 +26,27 @@ tables.forEach(table => {
 
 function handle_th(elmnt) {
     console.log(elmnt);
+    cm_th.style.left = posX + "px";
+    cm_th.style.top = posY + "px";
+    cm_th.style.display = "block";
 }
 
 function handle_td(elmnt) {
     console.log(elmnt);
+    cm_td.style.left = posX + "px";
+    cm_td.style.top = posY + "px";
+    cm_td.style.display = "grid";
 }
+
+
+cm_th.querySelector('div').addEventListener('mouseleave', function(e) {
+    if(e.relatedTarget.tagName !== "BUTTON"){
+        cm_th.style.display = "none";
+    }
+});
+
+cm_td.querySelector('div').addEventListener('mouseleave', function(e) {
+    if(e.relatedTarget.tagName !== "BUTTON"){
+        cm_td.style.display = "none";
+    }
+});
